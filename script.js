29025264873c0d8e985bed65bb166117cba0dca5
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-18 13:26:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // prevent form submission, we just want to show the alert
+
+    const firstName = document.querySelector('input[name="First Name"]').value;
+    const lastName = document.querySelector('input[name="Last Name"]').value;
+    const phoneNumber = document.querySelector('input[name="Phone Number"]').value;
+    const emailID = document.querySelector('input[name="Email ID"]').value;
+
+    const result = `
+        First Name: ${firstName}
+        Last Name: ${lastName}
+        Phone Number: ${phoneNumber}
+        Email ID: ${emailID}
+    `;
+
+    alert(result);
+});
